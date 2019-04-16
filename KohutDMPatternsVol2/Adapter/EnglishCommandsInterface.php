4624318d1c9f93253a6a41a5342581e77e08db4f
KohutDM <Kogutdmytro@gmail.com>
2019-04-16 17:22:34 +0300

    Adapter

---
+++ KohutDMPatternsVol2/Adapter/EnglishCommandsInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace school5\KohutDMPatternsVol2\Adapter;
+
+interface EnglishCommandsInterface
+{
+    public function Attack();
+
+    public function Defence();
+}
---
+++ KohutDMPatternsVol2/Adapter/FrenchSoldierAdapter.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace school5\KohutDMPatternsVol2\Adapter;
+
+class FrenchSoldierAdapter implements EnglishCommandsInterface
+{
+    private $frenchSoldier;
+
+    public function __construct(FrenchSoldier $frenchSoldier)
+    {
+        $this->frenchSoldier = $frenchSoldier;
+    }
+
+    public function attack()
+    {
+       return $this->frenchSoldier->attaque();
+    }
+
+    public function defence()
+    {
+       return $this->frenchSoldier->laDefense();
+    }
+}
