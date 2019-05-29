b636c58a289cbdb9bf37e2b9d360ffc93a222682
KohutDM <47785589+KohutDM@users.noreply.github.com>
2019-05-29 12:46:58 +0300

    Delete Collection.php

--- ForGit/magento/app/code/local/School/OnlineGaming/Model/Resource/Eavgames/Collection.php
+++
@@ -1,8 +0,0 @@
-<?php
-class School_OnlineGaming_Model_Resource_Eavgames_Collection extends Mage_Eav_Model_Entity_Collection_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init('onlinegaming/eavgames');
-    }
-}
