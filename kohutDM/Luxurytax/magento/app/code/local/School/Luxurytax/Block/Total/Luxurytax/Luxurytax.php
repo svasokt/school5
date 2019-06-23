3b5d3ad94c677faadb7e975a647dc0134a8fb72a
KohutDM <Kogutdmytro@gmail.com>
2019-06-23 14:59:36 +0300

    Luxury Tax

---
+++ kohutDM/Luxurytax/magento/app/code/local/School/Luxurytax/Block/Total/Luxurytax/Luxurytax.php
@@ -0,0 +1,18 @@
+<?php
+class School_Luxurytax_Block_Total_Luxurytax_Luxurytax extends Mage_Sales_Block_Order_Totals
+{
+	public function initTotals()
+    {
+	    $order = $this->getParentBlock()->getSource();
+    	if($order->getLuxuryTaxAmount() > 0){
+        	$this->getParentBlock()->addTotal(new Varien_Object(array(
+        	'code' => 'luxury Tax',
+            'value' => $order->getLuxuryTaxAmount(),
+            'base_value' => $order->getBaseLuxuryTaxAmount(),
+            'label' => Mage::helper('school_luxurytax')
+                        ->__(Mage::getStoreConfig('luxurytax_options/luxurytax_group/luxurytax_fetch_label')),
+            )),'subtotal');
+    	}
+    	return $this;
+	}
+}
---
+++ kohutDM/Luxurytax/magento/app/code/local/School/Luxurytax/Model/Total/Luxurytax/Invoice.php
@@ -0,0 +1,18 @@
+<?php
+class School_Luxurytax_Model_Total_Luxurytax_Invoice extends Mage_Sales_Model_Order_Invoice_Total_Abstract
+{
+	public function collect(Mage_Sales_Model_Order_Invoice $invoice)
+    {
+	$order = $invoice->getOrder();
+	$luxuryTaxAmount = $order->getLuxuryTaxAmount();
+	$baseLuxuryTaxAmount = $order->getBaseLuxuryTaxAmount();
+
+	$invoice->setLuxuryTaxAmount($luxuryTaxAmount);
+	$invoice->setBaseLuxuryTaxAmount($baseLuxuryTaxAmount);
+
+	$invoice->setGrandTotal($invoice->getGrandTotal() + $luxuryTaxAmount);
+    $invoice->setBaseGrandTotal($invoice->getBaseGrandTotal() + $baseLuxuryTaxAmount);
+
+    return $this;
+	}
+}
